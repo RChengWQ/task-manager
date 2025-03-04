016b1ca459967e45b1b2b5a2ddb5845832ba859b
Ricky Cheng <56271724+RChengWQ@users.noreply.github.com>
2025-03-04 15:22:36 +0000

    chore: Add routes.js to aggregate all routes
    - Create routes.js to centralize and manage all feature routes.

---
+++ server/utils/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const taskRoutes = require("../features/tasks/task-routes"); // Importing router
+
+router.use("/tasks", taskRoutes); // Mounting the task router
+
+module.exports = router; // Exporting aggregated router for use in server.js
