eed4489f0e7ed8bcd4e9b1718cd354003f4f4781
Ricky Cheng <56271724+RChengWQ@users.noreply.github.com>
2025-03-04 15:24:58 +0000

    refactor: remove `/tasks` prefix from route URLs
    - Remove the `/tasks` part of the URL in task-routes to streamline the API structure.

--- server/features/tasks/task-routes.js
+++ server/features/tasks/task-routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('task-controller');
 
-router.post('/tasks', taskController.createTask);
-router.delete('/tasks/:taskId', taskController.deleteTask);
-router.put('/tasks/:taskId/move/:listId', taskController.moveTask);
-router.get('/tasks/list/:listId', taskController.getAllTasksByListId);
-router.get('/tasks/:taskId', taskController.getTask);
-router.put('/tasks/title/:taskId', taskController.updateTaskTitle);
-router.put('/tasks/desc/:taskId', taskController.updateTaskDescription);
+router.post('/', taskController.createTask);
+router.delete('/:taskId', taskController.deleteTask);
+router.put('/:taskId/move/:listId', taskController.moveTask);
+router.get('/list/:listId', taskController.getAllTasksByListId);
+router.get('/:taskId', taskController.getTask);
+router.put('/title/:taskId', taskController.updateTaskTitle);
+router.put('/desc/:taskId', taskController.updateTaskDescription);
 
 module.exports = router();
