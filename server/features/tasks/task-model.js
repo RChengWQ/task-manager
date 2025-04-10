b486c47fc37d367cc04ef7c894c62fd3e1c54764
Ricky Cheng <56271724+RChengWQ@users.noreply.github.com>
2025-04-10 15:28:05 +0100

    Chore: Replace createdAt field with timestamps.
    - Replace createdAt field with mongoose timestamps, to automatically handle createdAt and updatedAt.

--- server/features/tasks/task-model.js
+++ server/features/tasks/task-model.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String },
+    title: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String 
+    },
     // Each task is linked to a list, depending on which stage it is on.
-    listId: { type: mongoose.Schema.Types.ObjectId, ref: 'List', required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+    listId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'List', 
+        required: true 
+    }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Task', taskSchema);
