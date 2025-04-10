8e21cfb96fbb1fed73c48e3904bfaeaed18f317d
Ricky Cheng <56271724+RChengWQ@users.noreply.github.com>
2025-04-10 15:31:00 +0100

    chore(list): Remove unnecessary taskId field

--- server/features/lists/list-model.js
+++ server/features/lists/list-model.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const listSchema = new mongoose.Schema({
-    name: { type: String, required: true, enum: ['todo', 'in-progress', 'completed']},
-    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+    title: { type: String, required: true}
 });
 
 module.exports = mongoose.model('List', listSchema);
